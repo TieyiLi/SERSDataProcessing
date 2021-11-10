8809ea4a0a5f3fd48c419a95d581a1514f45a168
TieyiLi <54391794+TieyiLi@users.noreply.github.com>
2021-11-09 22:12:45 -0800

    Update AdditionalProcessing.py

--- AdditionalProcessing.py
+++ AdditionalProcessing.py
@@ -3,8 +3,9 @@ from Utils import Utils
 
 
 class AdditionalProcessing(Utils):
-
+    
     def clustering_summary(self, cluster_pred):
+        
         cluster_size_summary = []
         pred_set = set(cluster_pred)
         for s in pred_set:
@@ -13,15 +14,29 @@ class AdditionalProcessing(Utils):
         return cluster_size_summary
 
 
-    def relabel_by_purity(self, cluster_pred, y_train):
-        new_label = np.zeros_like(y_train)
+    def relabel_by_purity(self, cluster_pred, ori_label):
+        """
+        This function relabels the data in the way that only cluster exlusively contains diseased
+        samples is labeled as diseased.
+      
+        Parameters
+        ----------
+        cluser_pred: clustering predictions with the shape of (number of spectra, )
+        ori_label: original label to be relabeled, usually using the one labeling normal or diseased
+        
+        Returns
+        -------
+        New label with the same shape
+        """
+        
+        new_label = np.zeros_like(ori_label)
         total_cluster_num = 0
         total_interest_num = 0
-        L = len(y_train)
+        L = len(ori_label)
         pred_set = set(cluster_pred)
         for s in pred_set:
             idx_s = [i for i in range(L) if cluster_pred[i] == s]
-            if np.all(y_train[idx_s] == 1):
+            if np.all(ori_label[idx_s] == 1):
                 new_label[idx_s] = 1
                 total_cluster_num += 1
                 total_interest_num += len(idx_s)
@@ -33,7 +48,21 @@ class AdditionalProcessing(Utils):
 
 
     def relabel_by_ratio(self, cluster_pred, ori_label, percentage=0.5):
-
+        """
+        This function relabels the data in the way that cluster contains diseased spectra fraction 
+        more than a percentage threshold is labeled as diseased.
+      
+        Parameters
+        ----------
+        cluser_pred: clustering predictions with the shape of (number of spectra, )
+        ori_label: original label to be relabeled, usually using the one labeling normal or diseased
+        percentage: threshold percentage of diseased spectra fraction
+        
+        Returns
+        -------
+        New label with the same shape
+        """
+        
         if set(ori_label) != {0, 1}:
             raise ValueError('Relabel can not be applied to multi-labels!')
 
@@ -62,6 +91,9 @@ class AdditionalProcessing(Utils):
         return new_label
 
     def input_data(self, data_path, output='content'):
+        """
+        Read data dictionary which is ready to be analyzed.
+        """
         data_dict = np.load(data_path, allow_pickle=True).item()
         if output == 'content':
             X = data_dict['data_matrix']
@@ -76,12 +108,25 @@ class AdditionalProcessing(Utils):
         else:
             raise ValueError('Option is not defined!')
 
-    def test_summary_spec_base(self, test_individual_label, y_pred):
-        individual = set(test_individual_label)
+    def test_summary_spec_base(self, test_sample_label, y_pred):
+        """
+        This function generates a summary of test with counting the number of spectra instead of 
+        mappings (vesicles or particles).
+      
+        Parameters
+        ----------
+        test_sample_label: labels labeling each sample in the test dataset
+        y_pred: predictions of a model running on the test dataset
+        
+        Returns
+        -------
+        None
+        """
+        individual = set(test_sample_label)
         pred_type = set(y_pred)
         for i in individual:
             print('Individual %d:' % i)
-            idx = [j for j in range(len(test_individual_label)) if test_individual_label[j] == i]
+            idx = [j for j in range(len(test_sample_label)) if test_sample_label[j] == i]
             pred_i = y_pred[idx]
             for t in pred_type:
                 num = len(pred_i[pred_i == t])
@@ -89,8 +134,22 @@ class AdditionalProcessing(Utils):
                 print('(%.3f)' % (num / len(pred_i)))
 
     def test_summary_map_base(self, test_sample_label, map_index, y_pred):
+        """
+        This function generates a summary of test with counting the number of mappings (vesicles
+        or particles) instead of spectra.
+      
+        Parameters
+        ----------
+        test_sample_label: labels labeling each sample in the test dataset
+        map_index: mapping name of each spectra stored in the data dictionary
+        y_pred: predictions of a model running on the test dataset
+        
+        Returns
+        -------
+        None
+        """
+        
         sample_type = set(test_sample_label)
-        confusion_matrix = np.empty(shape=(len(sample_type), len(sample_type)), dtype=np.int32)
         s = 0
         for i in sample_type:
             content_summary = []
@@ -109,9 +168,7 @@ class AdditionalProcessing(Utils):
             w = 0
             for n in set(content_summary):
                 len_n = len([v for v in range(len(content_summary)) if content_summary[v] == n])
-                confusion_matrix[s, w] = len_n
                 w += 1
                 print('    Prediction of %d: %d' % (int(n), len_n), end='')
                 print('(%.3f)' % (len_n / len(content_summary)))
             s += 1
-        return confusion_matrix

53666e4bd1d635f3c18030b1e2ee67a973090ef2
TieyiLi <54391794+TieyiLi@users.noreply.github.com>
2021-11-11 12:09:36 -0800

    Add files via upload

---
+++ Tools_V2/AdditionalProcessing.py
@@ -0,0 +1,182 @@
+"""
+===============================
+Additional processing functions 
+===============================
+This class contains additional useful functions such as relabeling, summarizing 
+test results and so on.
+"""
+
+import numpy as np
+from Tools.Utils import Utils
+
+
+class AdditionalProcessing(Utils):
+    
+    def clustering_summary(self, cluster_pred):
+        
+        cluster_size_summary = []
+        pred_set = set(cluster_pred)
+        for s in pred_set:
+            idx_s = [i for i in range(len(cluster_pred)) if cluster_pred[i] == s]
+            cluster_size_summary.append(len(idx_s))
+        return cluster_size_summary
+
+
+    def relabel_by_purity(self, cluster_pred, ori_label):
+        """
+        This function relabels the data in the way that only cluster exlusively contains diseased
+        samples is labeled as diseased.
+      
+        Parameters
+        ----------
+        cluser_pred: clustering predictions with the shape of (number of spectra, )
+        ori_label: original label to be relabeled, usually using the one labeling normal or diseased
+        
+        Returns
+        -------
+        New label with the same shape
+        """
+        
+        new_label = np.zeros_like(ori_label)
+        total_cluster_num = 0
+        total_interest_num = 0
+        L = len(ori_label)
+        pred_set = set(cluster_pred)
+        for s in pred_set:
+            idx_s = [i for i in range(L) if cluster_pred[i] == s]
+            if np.all(ori_label[idx_s] == 1):
+                new_label[idx_s] = 1
+                total_cluster_num += 1
+                total_interest_num += len(idx_s)
+
+        print('%d/%d specific clusters' % (total_cluster_num, len(pred_set)))
+        print('%d/%d interest instances' % (total_interest_num, L))
+
+        return new_label
+
+
+    def relabel_by_ratio(self, cluster_pred, ori_label, percentage=0.5):
+        """
+        This function relabels the data in the way that cluster contains diseased spectra fraction 
+        more than a percentage threshold is labeled as diseased.
+      
+        Parameters
+        ----------
+        cluser_pred: clustering predictions with the shape of (number of spectra, )
+        ori_label: original label to be relabeled, usually using the one labeling normal or diseased
+        percentage: threshold percentage of diseased spectra fraction
+        
+        Returns
+        -------
+        New label with the same shape
+        """
+        
+        if set(ori_label) != {0, 1}:
+            raise ValueError('Relabel can not be applied to multi-labels!')
+
+        if not isinstance(cluster_pred, np.ndarray):
+            cluster_pred = np.array(cluster_pred)
+        if not isinstance(ori_label, np.ndarray):
+            ori_label = np.array(ori_label)
+
+        new_label = np.empty(shape=cluster_pred.shape)
+        cluster_set = set(cluster_pred)
+        for set_i in cluster_set:
+            idx = [i for i in range(len(cluster_pred)) if cluster_pred[i] == set_i]
+
+            group_label_i = ori_label[idx]
+            types = list(set(group_label_i))
+            if len(types) == 1:
+                new_label[idx] = types[0]
+            else:
+                num_tot = len(group_label_i)
+                num_p = len(group_label_i[group_label_i == 1])
+                if num_p >= percentage * num_tot:
+                    new_label[idx] = 1
+                else:
+                    new_label[idx] = 0
+
+        return new_label
+
+    def input_data(self, data_path, output='content'):
+        """
+        Read data dictionary which is ready to be analyzed.
+        """
+        data_dict = np.load(data_path, allow_pickle=True).item()
+        if output == 'content':
+            X = data_dict['data_matrix']
+            label = data_dict['label']
+            group = data_dict['group']
+            sample_name = data_dict['sample_name']
+            raman_shift = data_dict['raman_shift']
+            map_index = data_dict['map_index']
+            return X, label, group, sample_name, raman_shift, map_index
+        elif output == 'dict':
+            return data_dict
+        else:
+            raise ValueError('Option is not defined!')
+
+    def test_summary_spec_base(self, test_sample_label, y_pred):
+        """
+        This function generates a summary of test with counting the number of spectra instead of 
+        mappings (vesicles or particles).
+      
+        Parameters
+        ----------
+        test_sample_label: labels labeling each sample in the test dataset
+        y_pred: predictions of a model running on the test dataset
+        
+        Returns
+        -------
+        None
+        """
+        individual = set(test_sample_label)
+        pred_type = set(y_pred)
+        for i in individual:
+            print('Individual %d:' % i)
+            idx = [j for j in range(len(test_sample_label)) if test_sample_label[j] == i]
+            pred_i = y_pred[idx]
+            for t in pred_type:
+                num = len(pred_i[pred_i == t])
+                print('    predicted as %d: %d' % (t, num), end='')
+                print('(%.3f)' % (num / len(pred_i)))
+
+    def test_summary_map_base(self, test_sample_label, map_index, y_pred):
+        """
+        This function generates a summary of test with counting the number of mappings (vesicles
+        or particles) instead of spectra.
+      
+        Parameters
+        ----------
+        test_sample_label: labels labeling each sample in the test dataset
+        map_index: mapping name of each spectra stored in the data dictionary
+        y_pred: predictions of a model running on the test dataset
+        
+        Returns
+        -------
+        None
+        """
+        
+        sample_type = set(test_sample_label)
+        s = 0
+        for i in sample_type:
+            content_summary = []
+            print('Individual %d:' % i)
+            sample_idx = [j for j in range(len(test_sample_label)) if test_sample_label[j] == i]
+            pred_i = y_pred[sample_idx]
+
+            map_for_sample = [map_index[s] for s in sample_idx]
+            map_type = set(map_for_sample)
+            for m in map_type:
+                map_idx = [u for u in range(len(map_for_sample)) if map_for_sample[u] == m]
+                pred_m = pred_i[map_idx]
+                most_common_ele = np.argmax(np.bincount(pred_m))
+                content_summary.append(most_common_ele)
+
+            w = 0
+            for n in set(content_summary):
+                len_n = len([v for v in range(len(content_summary)) if content_summary[v] == n])
+                w += 1
+                print('    Prediction of %d: %d' % (int(n), len_n), end='')
+                print('(%.3f)' % (len_n / len(content_summary)))
+            s += 1
---
+++ Tools_V2/Utils.py
@@ -0,0 +1,209 @@
+"""
+=========================================================
+Class for spectrum-related calculation and evaluation
+=========================================================
+
+This class includes functions for sers spectra baseline subtraction, 
+smoothing or noise reduction, normalization and quality estimation as
+well as visualizations.
+"""
+
+from matplotlib import ticker, patches
+import numpy as np
+from scipy.signal import savgol_filter
+
+
+class Utils:
+
+    def norm_signal(self, xx):
+        """
+        Normalizing a single spectrum
+        Parameters
+        ----------
+        xx: a single spectrum represented by 1D numpy array (since the 1st dimension-
+        Raman shifts of all spectra are the same therefore this dimension is omitted)
+
+        Returns
+        -------
+        Normalized spectrum
+        """
+        return (xx - min(xx)) / (max(xx) - min(xx))
+
+    def normalize(self, X, feature_range=(0, 1)):
+        """
+        Batch normalization of spectra
+        Parameters
+        ----------
+        X: Matrix with each row as a spectrum represented by 1D numpy array
+        feature_range: compression value range
+
+        Returns
+        -------
+        Normalized spectra collection
+        """
+        if X.ndim != 2:
+            raise ValueError('X must be 2D matrix with instances X features!')
+        X_std = (X - np.min(X, axis=1, keepdims=True)) / \
+                (np.max(X, axis=1, keepdims=True) - np.min(X, axis=1, keepdims=True))
+        X_normed = X_std * (max(feature_range) - min(feature_range)) + min(feature_range)
+        return X_normed
+
+
+    def baseline_als(self, x, lam=1e4, p=0.005, niter=10):
+        """
+        This function computes the estimated baseline alignments of spectra.
+
+        Parameters
+        ----------
+        x: single spectrum represented by 1D numpy array
+        lam: aligning factor, must be 1e4 currently
+        p: aligning factor, must be 0.005 currently
+        niter: iteration rounds of fitting, using 10 currently
+
+        Returns
+        -------
+        Estimated baseline of a single spectrum represented by 1D numpy array.
+        """
+
+        L = len(x)
+        D = csc_matrix(np.diff(np.eye(L), 2))
+        w = np.ones(L)
+        baseline = 0
+        for i in range(niter):
+            W = spdiags(w, 0, L, L)
+            Z = W + lam * D.dot(D.transpose())
+            baseline = linalg.spsolve(Z, w * x)
+            w = p * (x > baseline) + (1 - p) * (x < baseline)
+        return baseline
+
+    def baseline_sub(self, X, **kwargs):
+        """
+        Batch baseline subtraction of spectra
+        Parameters
+        ----------
+        X: Matrix with each row as a spectrum represented by 1D numpy array
+
+        Returns
+        -------
+        Baseline subtracted spectra collection
+        """
+        if X.ndim != 2:
+            raise ValueError('X must be 2D matrix with instances X features!')
+        X_baseline = np.zeros(shape=X.shape)
+        init = 0
+        for xx in X:
+            xx_baseline = self.baseline_als(xx, **kwargs)
+            X_baseline[init, :] = xx_baseline
+            init += 1
+        return X - X_baseline
+
+    def smoothing(self, X, window_length=7, polyorder=1):
+        """
+        Batch smoothing of spectra collection or noise removal of spectra
+        collection based on Savitzky-Golay filtering
+        Parameters
+        ----------
+        X: Matrix with each row as a spectrum represented by 1D numpy array
+        window_length: number of neighboring points used for local fitting
+        polyorder: order of polynomial function for the fitting.
+
+        Returns
+        -------
+        Smoothed spectra collection
+        """
+        if X.ndim != 2:
+            raise ValueError('X must be 2D matrix with instances X features!')
+        return savgol_filter(X, window_length, polyorder, axis=1)
+
+    def check_single_spike(self, xx):
+        """
+        Find cosmic ray of a single spectrum (not stable and efficient
+        currently, need further improvement)
+
+        Parameters
+        ----------
+        xx: a single spectrum represented by 1D numpy array
+
+        Returns
+        -------
+        Judgement of whether a single spectrum contains cosmic ray
+        """
+        xx_norm = self.norm_signal(xx)
+        peaks, _ = find_peaks(xx_norm, height=0.2)
+        if len(peaks) <= 1:
+            '''single spike'''
+            return False
+        if max(peaks) - min(peaks) <= 3:
+            '''2 closely-connected spikes'''
+            return False
+        if max(np.gradient(xx_norm)) >= 0.2:
+            '''Spike hidden in signals'''
+            return False
+        return True
+
+    def estimate_snr(self, xx):
+        """
+        Estimate the quality in terms of signal-to-noise ratio of a single 
+        spectrum
+        Parameters
+        ----------
+        xx: a single spectrum represented by 1D numpy array
+
+        Returns
+        -------
+        A quantified value of the quality of a single spectrum
+        """
+
+        xx_base_sub = xx - self.baseline_als(xx, lam=1e4, p=0.005)
+        xx_signal = savgol_filter(xx_base_sub, window_length=7, polyorder=1)
+        
+        # The maximal value of the spectrum as the 'S' value
+        S = max(xx_signal)
+        
+        # The average of noise extracted by savgol filter as the 'N' value
+        N = np.mean(abs(xx_base_sub - xx_signal))
+        return S / N, xx_signal
+    
+    
+class Visualization:
+
+    def __init__(self):
+        self.cmap = ['b', 'orange', 'g', 'r', 'purple', 'peru', 'gold', 'skyblue', 'brown', 'salmon', 'darkred',
+                     'orangered', 'sienna', 'wheat']
+        # self.cmap = cm.get_cmap('tab10')
+
+    def __get_cov_ellipse(self, cov, centre, nstd, **kwargs):
+        # Return a matplotlib Ellipse patch representing the covariance matrix
+        # cov centred at centre and scaled by the factor nstd.
+
+        # Find and sort eigenvalues and eigenvectors into descending order
+        eigvals, eigvecs = np.linalg.eigh(cov)
+        order = eigvals.argsort()[::-1]
+        eigvals, eigvecs = eigvals[order], eigvecs[:, order]
+
+        # The anti-clockwise angle to rotate our ellipse by
+        vx, vy = eigvecs[:, 0][0], eigvecs[:, 0][1]
+        theta = np.arctan2(vy, vx)
+
+        # Width and height of ellipse to draw
+        width, height = 2 * nstd * np.sqrt(eigvals)
+
+        return patches.Ellipse(xy=centre, width=width, height=height, angle=np.degrees(theta), **kwargs)
+
+    def __split_group(self, label):
+        for num in list(set(label)):
+            index = [i for i, x in enumerate(label) if x == num]
+            yield [index, num]
+
+    def scatter_plot(self, ax, x, y, label, marker='o', conf_eval=False):
+        for index in self.__split_group(label):
+            if index[1] != -1:
+                xx, yy = x[index[0]], y[index[0]]
+                ax.scatter(xx, yy, c=self.cmap[index[1] % len(self.cmap)], marker=marker, label=index[1], s=10, lw=0)
+                if conf_eval:
+                    conf_ellipse = self.__get_cov_ellipse(np.cov(xx, yy), (np.mean(xx), np.mean(yy)), nstd=2.0,
+                                                        ec=self.cmap[index[1]], fill=False, alpha=0.6)
+                    ax.add_artist(conf_ellipse)
+            else:
+                xx, yy = x[index[0]], y[index[0]]
+                ax.scatter(xx, yy, c='gray', label=index[1], s=8, alpha=0.4, lw=0)

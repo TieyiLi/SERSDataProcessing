67455f2bfb93e099d934b47d86c64dbce7db4823
TieyiLi <54391794+TieyiLi@users.noreply.github.com>
2022-07-15 11:03:54 -0700

    Update RUN.py

--- Auto/RUN.py
+++ Auto/RUN.py
@@ -3,18 +3,17 @@ import pyautogui as agent
 
 #================================AUTO MEASUREMENT==========================#
 #----------------------DO check button positions before running!!!---------#
-##check_button_positions()
+#check_button_positions()
 #---------------------------------Start running----------------------------#
 #REQIUREMENTS:
-# 1) Coarse mapping and fine mapping templetes are open;
+# 1) Coarse mapping (1st) and fine mapping (2nd) templetes are open;
 # 2) Staying at origion (0, 0) with good focus;
 # 3) Laser and grating are ready to use without laser or grating switching;
 # 4) Crosshairs and scalebar in Video window are recommended to be closed.
-# 5) Turn off administrator confirmation notification
+# 5) Turn off administrator confirmation notification.
 
-save_folder_path = r'C:\Users\Tieyi Li\Auto run test v2'
-sample_name_str = 'Cnt5Exo'
-area_cor = [[0, 0], [1990, 1785]]
-z_range = [-20, 15]
+save_folder_path = r'C:\Users\Siddharth Srivastava\eye_exo\round3\09_01'
+sample_name_str = '09_01'
+area_cor = [[0, 0], [2500, 2200]]
 
-Run(save_folder_path, sample_name_str, area_cor, z_range)
+Run(save_folder_path, sample_name_str, area_cor, coarse_id_from=1)
